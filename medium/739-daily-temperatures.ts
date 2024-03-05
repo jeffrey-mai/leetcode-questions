00233b0774df824288d7974a0c8825fb0e9eec6e
Final4Head <jeffreymai3000@gmail.com>
2024-03-05 13:21:05 -0500

    reviewed 739-daily-temperatures

--- medium/739-daily-temperatures.ts
+++ medium/739-daily-temperatures.ts
@@ -1,14 +1,24 @@
 function dailyTemperatures(temperatures: number[]): number[] {
-  const results = Array(temperatures.length).fill(0);
-  const stack: number[] = [];
-  for(let i = 0; i < temperatures.length; i++) {
-    while(stack.length && temperatures[i] > temperatures[stack[stack.length - 1]]) {
-      const temp = stack.pop();
-      if(temp !== undefined) results[temp] = i - temp;
+  // const results = Array(temperatures.length).fill(0);
+  // const stack: number[] = [];
+  // for(let i = 0; i < temperatures.length; i++) {
+  //   while(stack.length && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+  //     const temp = stack.pop();
+  //     if(temp !== undefined) results[temp] = i - temp;
+  //   }
+  //   stack.push(i);
+  // }
+  // return results;
+
+  const stack = [], ans = new Array(temperatures.length).fill(0);
+  for(let i = 0; i < temperatures.length; i++){
+    while(stack.length && temperatures[stack[stack.length-1]] < temperatures[i]){
+      ans[stack[stack.length-1]] = i - stack[stack.length-1];
+      stack.pop();
     }
     stack.push(i);
   }
-  return results;
+  return ans;
 };
 
 console.log(dailyTemperatures([73,74,75,71,69,72,76,73]))
