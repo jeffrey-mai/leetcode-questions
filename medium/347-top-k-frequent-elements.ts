e478a71482f8e07ef08737774f69e00454e1c023
Final4Head <jeffreymai3000@gmail.com>
2024-03-07 15:30:14 -0500

    reviewed 347-top-k-frequent-elements

--- medium/347-top-k-frequent-elements.ts
+++ medium/347-top-k-frequent-elements.ts
@@ -1,21 +1,30 @@
 // 
 
 function topKFrequent(nums: number[], k: number): number[] {
-  const map = new Map(), ans = [];
-  for(const num of nums){
-    map.set(num, map.get(num) + 1 || 1);
+  // const map = new Map(), ans = [];
+  // for(const num of nums){
+  //   map.set(num, map.get(num) + 1 || 1);
+  // }
+  // for(const [key, value] of map){
+  //   ans.push([key, value]);
+  // }
+  // ans.sort((a, b) => b[1] - a[1]);
+  // console.log(ans);
+  // console.log(ans.slice(0, k))
+  // return ans.slice(0, k).map((x) => x[0]);
+
+  const freq: { [key: number]: number } = {};
+  for (const n of nums) {
+    freq[n] = freq[n] + 1 || 1;
   }
-  for(const [key, value] of map){
-    ans.push([key, value]);
-  }
-  ans.sort((a, b) => b[1] - a[1]);
-  console.log(ans);
-  console.log(ans.slice(0, k))
-  return ans.slice(0, k).map((x) => x[0]);
+  return Object.entries(freq)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, k)
+    .map(v => Number(v[0]));
 };
 
 console.log(topKFrequent([1,1,2,2,3,3], 2));
-// console.log(topKFrequent([1], 1));
-// console.log(topKFrequent([1, 2], 2));
+console.log(topKFrequent([1], 1));
+console.log(topKFrequent([1, 2], 2));
 
 export default topKFrequent;
