1665f8e324702e17aa0e5140e05357e545ada29b
Final4Head <jeffreymai3000@gmail.com>
2024-03-29 18:28:55 -0400

    python 217

---
+++ python/217-contains-duplicate.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+  def containsDuplicate(self, nums: List[int]) -> bool:
+    cache = set()
+    for num in nums:
+      if num in cache: return True 
+      cache.add(num)
+    return False
+  
+print(Solution().containsDuplicate([1,2,3,1]))
