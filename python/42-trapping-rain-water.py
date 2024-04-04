307dcf0d0e9eda1ccd115432fbfaa8a6545c4cdc
Final4Head <jeffreymai3000@gmail.com>
2024-04-04 14:55:27 -0400

    python 42

---
+++ python/42-trapping-rain-water.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+def trap(height: List[int]) -> int:
+    left, right, ans = 1, len(height) - 2, 0
+    maxLeft, maxRight = height[0], height[right + 1]
+    while left <= right:
+        if maxLeft <= maxRight:
+            if height[left] <= maxLeft:
+                ans += min(maxLeft, maxRight) - height[left]
+                left += 1
+            else:
+                maxLeft = height[left]
+        else:
+            if height[right] <= maxRight:
+                ans += min(maxLeft, maxRight) - height[right]
+                right -= 1
+            else:
+                maxRight = height[right]
+    return ans
+
+
+print(trap([4, 2, 0, 3, 2, 5]))
