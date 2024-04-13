08df761251c6c1037c1833ac97c01a836709a6ed
Final4Head <jeffreymai3000@gmail.com>
2024-04-13 14:00:45 -0400

    python 4

---
+++ python/4-median-of-two-sorted-arrays.py
@@ -0,0 +1,11 @@
+from typing import List
+from math import ceil
+
+def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
+    sortedList = sorted(nums1 + nums2)
+    n = len(sortedList)
+    if n%2 == 0:
+        median = (sortedList[n//2-1] + sortedList[n//2])/2
+    else:
+        median = sortedList[ceil(n/2)-1]
+    return median
