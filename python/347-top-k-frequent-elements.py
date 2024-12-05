422a60d19aa1dac3caa5348362d5e0cb26745bbb
Final4Head <jeffreymai3000@gmail.com>
2024-12-05 16:11:48 -0500

    347 python

--- python/347-top-k-frequent-elements.py
+++ python/347-top-k-frequent-elements.py
@@ -10,7 +10,6 @@ class Solution:
             else:
                 cache[num] = 1
         cache = dict(sorted(cache.items(), key=lambda x: x[1], reverse=True))
-        return list(cache.keys())[:k]
-
+        return list(cache.keys())[:k]         
 
 print(Solution().frequentElements([3, 0, 1, 0], 1))
