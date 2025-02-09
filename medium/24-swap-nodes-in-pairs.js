b82207ee2bba84ead4d4f56484e44d72fdaca5b3
Final4Head <jeffreymai3000@gmail.com>
2025-02-09 15:11:38 -0500

    24 js

---
+++ medium/24-swap-nodes-in-pairs.js
@@ -0,0 +1,25 @@
+function ListNode(val, next) {
+  this.val = (val===undefined ? 0 : val)
+  this.next = (next===undefined ? null : next)
+}
+
+var swapPairs = function(head) {
+  let dummy = new ListNode(0, head);
+  let prev = dummy, cur = head;
+
+  while (cur && cur.next) {
+    let npn = cur.next.next;
+    let second = cur.next;
+
+    second.next = cur;
+    cur.next = npn;
+    prev.next = second;
+
+    prev = cur;
+    cur = npn;
+  }
+
+  return dummy.next;    
+};
+
+console.log(swapPairs());
